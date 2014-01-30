3f020c31af523cf3e3b31f03ce91e650c154377f
evilexecutable <none>
2014-01-30 14:25:06 +0000

    Initial repo creation.

---
+++ main.lua
@@ -0,0 +1,33 @@
+print("|           |-----|\n\|           |     |\n\|           |-----|\n\|           |\n\|           |")
+print(" ------")
+print("Welcome to LuaProtect!")
+
+local function start()
+print("\n\n\========Choose your poison.=======")
+print(" a): Clear Browsers (Cache, profiles, etc) WARNING: You'll lose bookmarks. \n\ b): Remove cache only\n\ c): Write 0's to C drives free space.")
+choice = io.read()
+end
+
+start()
+
+if choice == "b" then
+	print("Detecting Browsers...")
+	local responseie = os.execute("cd C:\\Program Files\\Internet Explorer\\")
+		if responseie == 0 then
+			print("Internet Explorer found... \n\Deleting cache files...")
+			os.execute("del %temp%\\*")
+			print("Done!")
+		else
+			print("IE not found, are you sure you're running on Windows?")
+		end
+
+	local responsechrome = os.execute("cd C:\\Program Files\\Google\\Chrome\\Application\\")
+		if responsechrome == 0 then
+			print("Chrome found... Deleting cache files...")
+			os.execute("del %localappdata%\\Google\\Chrome\\User Data\\Default\\Cache\\*")
+			print("Done!")
+		else
+			print("Chrome not found, skipping...")
+		end
+	
+end
