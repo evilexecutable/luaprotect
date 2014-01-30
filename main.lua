138f318d487f31b2aa651fcf97719dc665155634
evilexecutable <none.none>
2014-01-30 14:32:32 +0000

    Add comments.

--- main.lua
+++ main.lua
@@ -12,19 +12,20 @@ start()
 
 if choice == "b" then
 	print("Detecting Browsers...")
-	local responseie = os.execute("cd C:\\Program Files\\Internet Explorer\\")
+	local responseie = os.execute("cd C:\\Program Files\\Internet Explorer\\")	--Check for internet explorer.
 		if responseie == 0 then
 			print("Internet Explorer found... \n\Deleting cache files...")
-			os.execute("del %temp%\\*")
+			os.execute("del %temp%\\*") --Execute del command.
 			print("Done!")
 		else
 			print("IE not found, are you sure you're running on Windows?")
 		end
+	
 
-	local responsechrome = os.execute("cd C:\\Program Files\\Google\\Chrome\\Application\\")
+	local responsechrome = os.execute("cd C:\\Program Files\\Google\\Chrome\\Application\\") --Check for Google Chrome.
 		if responsechrome == 0 then
 			print("Chrome found... Deleting cache files...")
-			os.execute("del %localappdata%\\Google\\Chrome\\User Data\\Default\\Cache\\*")
+			os.execute("del %localappdata%\\Google\\Chrome\\User Data\\Default\\Cache\\*")	--Execute del command.
 			print("Done!")
 		else
 			print("Chrome not found, skipping...")
