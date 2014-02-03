b0f59ee70797024e91d9566b5f0543282e3e1299
evilexecutable <none.none>
2014-02-03 16:18:56 +0000

    Allow user to choose how many wipes to do.

--- drivewipe.lua
+++ drivewipe.lua
@@ -15,10 +15,21 @@
 --  with this program; if not, write to the Free Software Foundation, Inc.,
 --  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ---------------------------------------------------------------------------------
-
+	
 	print("Enter drive letter you wish to wipe:")
 	local drivechoice = io.read():upper()
-	print("Cipher is now wiping the free space on the drive. This will take a while.")
-	os.execute("cipher /w:" .. (drivechoice))
+	
+	print("How many times do you wish to wipe?\n\The more wipes, the longer it will take but more secure.")
+	local wipecount = io.read()
+	
+	local count = 0
+	
+	while count < wipecount do
+		print("Cipher is now wiping the free space on the drive. This will take a while.")
+		os.execute("cipher /w:" .. (drivechoice))
+		print("Done " .. count .. " passes.")
+		count = count + 1
+	end
+	
 	print("Drive wiped.")
 	dofile("main.lua")
