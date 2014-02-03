ac167cb5dd44b9ea205af24d2dd7bb4563e1fb1c
evilexecutable <none.none>
2014-02-03 15:58:04 +0000

    Split files for easier maintenance.

--- drivewipe.lua
+++ drivewipe.lua
@@ -16,29 +16,9 @@
 --  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ---------------------------------------------------------------------------------
 
-print("|           |-----|\n\|           |     |\n\|           |-----|\n\|           |\n\|           |")
-print(" ------")
-print("Welcome to LuaProtect!")
-
-function start()
-print("\n\n\========Choose your poison=======")
-print(" a): Clear Installed Browsers (Cache, profiles) WARNING: You'll lose bookmarks.  b): Remove cache only\n\ c): Write 0's to C drives free space.")
-print(" (Please all applications are closed) ")
-choice = io.read():lower()
-end
-
-start()
-
-if choice == "a" then
-	dofile("browserclean.lua")
-end
-
-if choice == "b" then
-	dofile("cacheclean.lua")
-end
-
-if choice == "c" then
-	dofile("drivewipe.lua")
-end
-
---start()
+	print("Enter drive letter you wish to wipe:")
+	local drivechoice = io.read():upper()
+	print("Cipher is now wiping the free space on the drive. This will take a while.")
+	os.execute("cipher /w:" .. (drivechoice))
+	print("Drive wiped.")
+	dofile("main.lua")
